01371587c4e2d6fe04fcac66faea80aae5a8085a
kanaMiyazato <kanamiyazato23@gmail.com>
2026-05-29 13:50:08 +0900

    コメント機能の修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 //使用するモデルの呼び出し
 use App\Models\User;
-// use App\Models\Novel;
-// use App\Models\Comment;
-// use App\Models\Bookmark;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -37,10 +34,9 @@ class UserController extends Controller
 
     public function user_update(Request $request, $user_id)
     {
-    // 1. 指定されたユーザーを取得（存在しない場合は404）
+    // 指定されたユーザーを取得（存在しない場合は404）
         $user = User::findOrFail($user_id);
 
-        // 2. バリデーションの実施
         $request->validate([
             'name' => ['required', 'string', 'min:2', 'max:20'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($user->id)],
@@ -56,7 +52,7 @@ class UserController extends Controller
 
         $user->save();
 
-        return redirect()->route('UserSettings.user_show', ['user_id' => $user->id])
+        return redirect()->route('usersettings.user_show', ['user_id' => $user->id])
         ->with('message', '変更が反映されました。');
     }
 }
